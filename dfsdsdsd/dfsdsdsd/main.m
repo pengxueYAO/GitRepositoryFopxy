93b226a0e4cb8eed469cc29aa17a71f206a5fc88
pengxueyao <494545801@qq.com>
2015-08-25 16:33:46 -1100

    创建一个新的工程

---
+++ dfsdsdsd/dfsdsdsd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dfsdsdsd
+//
+//  Created by pxy on 15/8/25.
+//  Copyright (c) 2015年 pxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
